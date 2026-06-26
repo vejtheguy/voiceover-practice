a7f461c27a5c1688ece6a3c66e129f3febe95d31
virgil jones <115853295+vejtheguy@users.noreply.github.com>
2026-06-26 12:52:07 -0700

    fix: footer link navigation (#28)

--- js/globalComponents.js
+++ js/globalComponents.js
@@ -10,6 +10,13 @@ document.addEventListener("DOMContentLoaded", () => {
       .then((response) => response.text())
       .then((data) => {
         footer.innerHTML = data;
+
+        const footerLinks = footer.querySelectorAll(".footer-resources a");
+        footerLinks.forEach((link) => {
+          const currentHref = link.getAttribute("href");
+          link.setAttribute("href", `${prefix}${currentHref}`);
+        });
+
         const yearDate = footer.querySelector("#year");
         if (yearDate) {
           yearDate.textContent = new Date().getFullYear();
