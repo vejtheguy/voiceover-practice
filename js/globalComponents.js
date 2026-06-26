820db1bf2412c26f2623d82472b4300c59b21bec
virgil elliot <115853295+vejtheguy@users.noreply.github.com>
2026-06-26 12:28:15 -0700

    fix: js folder loading

--- js/globalComponents.js
+++ js/globalComponents.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const footer = document.getElementById("global-footer");
+  const isHomePage =
+    window.location.pathname.endsWith("/") ||
+    window.location.pathname.endsWith("index.html");
+  const prefix = isHomePage ? "../" : "./";
 
   if (footer) {
-    fetch("/components/footer.html")
+    fetch(`${prefix}components/footer.html`)
       .then((response) => response.text())
       .then((data) => {
         footer.innerHTML = data;
