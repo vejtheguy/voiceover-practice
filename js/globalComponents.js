6cdf9fcbaaa2021ddaa08f92491e84e36f27f96f
virgil jones <115853295+vejtheguy@users.noreply.github.com>
2026-07-03 14:24:04 -0700

    feat: huge overhaul, main nav added (#39)
    Header navigation loads in via javascript. Footer nav updated. Styling V1.1 created. Reorganized folders for future lessons.

--- js/globalComponents.js
+++ js/globalComponents.js
@@ -1,21 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const header = document.getElementById("global-header");
   const footer = document.getElementById("global-footer");
-  const isHomePage =
-    window.location.pathname.endsWith("/") ||
-    window.location.pathname.endsWith("index.html");
-  const prefix = isHomePage ? "./" : "../";
+
+  if (header) {
+    fetch("/components/header.html")
+      .then((response) => response.text())
+      .then((data) => {
+        header.innerHTML = data;
+
+        const currentPath = window.location.pathname;
+        const headerLinks = header.querySelectorAll(".nav-links a");
+
+        headerLinks.forEach((link) => {
+          const currentHref = link.getAttribute("href");
+
+          if (currentHref === currentPath) {
+            link.setAttribute("aria-current", "page");
+          } else {
+            link.removeAttribute("aria-current");
+          }
+        });
+      });
+  }
 
   if (footer) {
-    fetch(`${prefix}components/footer.html`)
+    fetch("/components/footer.html")
       .then((response) => response.text())
       .then((data) => {
         footer.innerHTML = data;
-
-        const footerLinks = footer.querySelectorAll(".footer-resources a");
-        footerLinks.forEach((link) => {
-          const currentHref = link.getAttribute("href");
-          link.setAttribute("href", `${prefix}${currentHref}`);
-        });
 
         const yearDate = footer.querySelector("#year");
         if (yearDate) {
